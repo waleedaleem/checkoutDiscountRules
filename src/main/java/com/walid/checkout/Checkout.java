0a5cbac8eb452f956b72d4134f44b26536b089f1
Walid Moustafa <walid.moustafa@alexsolutions.com.au>
2017-09-04 20:11:41 +1000

    Add checkout class

---
+++ src/main/java/com/walid/checkout/Checkout.java
@@ -0,0 +1,22 @@
+package com.walid.checkout;
+
+import com.walid.checkout.inventory.InventoryItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Checkout {
+
+    private final List<CheckoutItem> checkoutItems = new ArrayList<>();
+
+    public void scan(String sku) {
+        // add scanned item to list
+        checkoutItems.add(new CheckoutItem(InventoryItem.getItem(sku)));
+    }
+
+    public double total() {
+        return checkoutItems.parallelStream()
+                .mapToDouble(CheckoutItem::getSalePrice)
+                .sum();
+    }
+}
