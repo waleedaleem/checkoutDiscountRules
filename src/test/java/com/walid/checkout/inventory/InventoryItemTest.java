dd89abd2f142a163f92f30ad1d31557efbe7b9a7
Walid Moustafa <walid.moustafa@alexsolutions.com.au>
2017-09-04 19:15:19 +1000

    Update InventoryItem test class

--- src/test/java/com/walid/checkout/inventory/InventoryItemTest.java
+++ src/test/java/com/walid/checkout/inventory/InventoryItemTest.java
@@ -2,15 +2,12 @@ package com.walid.checkout.inventory;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class InventoryItemTest {
     @Test
     public void initInventroyItemList() throws Exception {
         InventoryItem.initInventroyItemList();
-        assertThat(InventoryItem.INVENTORY_ITEM_LIST.get(0).getSku(), equalTo("ipd"));
-        assertThat(InventoryItem.INVENTORY_ITEM_LIST.get(0).getName(), equalTo("Super iPad"));
-        assertThat(InventoryItem.INVENTORY_ITEM_LIST.get(0).getPrice(), equalTo(549.99));
+        assertTrue(InventoryItem.INVENTORY_ITEM_LIST.contains(new InventoryItem("ipd", "Super iPad", 549.99)));
     }
 }
