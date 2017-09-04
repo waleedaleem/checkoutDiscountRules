c784e6e1f7e38e6a2e9be5c967525423b60c1212
Walid Moustafa <walid.moustafa@alexsolutions.com.au>
2017-09-04 19:17:57 +1000

    Add checkoutItem class

---
+++ src/main/java/com/walid/checkout/CheckoutItem.java
@@ -0,0 +1,22 @@
+package com.walid.checkout;
+
+import com.walid.checkout.inventory.InventoryItem;
+
+public class CheckoutItem {
+
+    private InventoryItem inventoryItem;
+    private double salePrice;
+
+    public CheckoutItem(InventoryItem inventoryItem) {
+        this.inventoryItem = inventoryItem;
+        this.salePrice = inventoryItem.getPrice();
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
+
+    public void adjustSalePrice(double salePrice) {
+        this.salePrice = salePrice;
+    }
+}
