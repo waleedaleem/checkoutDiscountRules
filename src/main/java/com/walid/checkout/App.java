ac52e661d24379b8ddead0b2cd411715294169c2
Walid Moustafa <walid.moustafa@alexsolutions.com.au>
2017-09-04 16:58:32 +1000

    create a project skeleton

---
+++ src/main/java/com/walid/checkout/App.java
@@ -0,0 +1,10 @@
+package com.walid.checkout;
+
+/**
+ * @author Walid Moustafa
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
